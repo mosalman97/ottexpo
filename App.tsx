40f658218b6930c344791d0b5768f60b1484b891
mosalman97 <mosalman1098@gmail.com>
2026-06-26 05:22:11 +0530

    setup nativewind with tailwind configuration

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+import "./global.css";
+
+export default function App() {
+  return (
+    <View className='flex-1 bg-yellow-500'>
+      <Text className='text-red-500'>App</Text>
+    </View>
+  )
+}
--- src/app/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import { useColorScheme } from 'react-native';
-
-export default function TabLayout() {
-  return (
-   <></>
-  );
-}
